233b87d6b6cdb94c2589e0741e6b771cd93b6bd2
Kvesten <pavelvoronov63@gmail.com>
2023-12-18 23:47:29 +0300

    Сделал табличку team

--- src/lib/components/Team.svelte
+++ src/lib/components/Team.svelte
@@ -1,1 +1,66 @@
-<h1>Team!</h1>
+<main>
+  <h2 class="title font-extrabold text-4xl">Команда</h2>
+  <div class="grid grid-cols-3 my-grid">
+    <div class="card">
+      <img src="img/IMG_2472_0.jpg" alt="" />
+      <div class="card_body">
+        <h3 class="text-2xl font-bold">Сергей Синица</h3>
+        <p class="card_desc">
+          Руководитель отдела веб-разработки, канд тех. наук, заместитель
+          директора.
+        </p>
+      </div>
+    </div>
+
+    <div class="card">
+      <img src="img/IMG_2539_0.jpg" alt="" />
+      <div class="card_body">
+        <h3 class="text-2xl font-bold">Роман Агабеков</h3>
+        <p class="card_desc">Руководитель отдела DevOps, директор.</p>
+      </div>
+    </div>
+
+    <div class="card">
+      <img src="img/IMG_2474_1.jpg" alt="" />
+      <div class="card_body">
+        <h3 class="text-2xl font-bold">Алексей Синица</h3>
+        <p class="card_desc">Руководитель отдела поддержки сайтов.</p>
+      </div>
+    </div>
+
+    <div class="card">
+      <img src="img/IMG_2522_0.jpg" alt="" />
+      <div class="card_body">
+        <h3 class="text-2xl font-bold">Дарья Бочкарёва</h3>
+        <p class="card_desc">
+          Руководитель отдела продвижения, контекстной рекламы и контент
+          поддержки сайтов.
+        </p>
+      </div>
+    </div>
+
+    <div class="card">
+      <img src="img/IMG_9971_16.jpg" alt="" />
+      <div class="card_body">
+        <h3 class="text-2xl font-bold">Ирина Торкунова</h3>
+        <p class="card_desc">Менеджер по работе с клиентами.</p>
+      </div>
+    </div>
+  </div>
+</main>
+
+<style>
+  .my-grid {
+    margin-left: 300px;
+    margin-right: 300px;
+  }
+  .card {
+    width: 280px;
+    margin: 50px;
+  }
+
+  .title {
+    text-align: center;
+    margin-top: 60px;
+  }
+</style>
